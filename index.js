0bd562c3d54f38aaf0b2c3cb606d0302f3b0d364
Marius Monkam <cloudmonde@yahoo.com>
2023-06-28 21:17:50 +0300

    fixing running python script as a child process

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
-//Specify the path to your Python file
-const pythonFilePath = "main.py";
+// Run the main.py script as a child process
+const pythonProcess = spawn("python", ["main.py"], { stdio: "inherit" });
 
-//execute the Python file using the child_process module
-exec(`python ${pythonFilePath}`, (error, stdout, stderr) => {
-  if (error) {
-    //if there was an error executing the python file, log the error message
-    console.error(`Error executing Python file:${error.message}`);
-    return;
-  }
-  if (stderr) {
-    //If the Python script returned an error, log the error message
-    console.error(`Python script returned an error: ${stderr}`);
-    return;
-  }
-  // Process the output from the Python script
-  console.log(`Python script output: ${stdout}`);
+// Listen for the Python script to exit
+pythonProcess.on("close", (code) => {
+  console.log(`Python script exited with code ${code}`);
 });
